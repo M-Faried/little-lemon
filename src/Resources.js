7cce3eddf92a42a686a6a3c725911046ccfae3f1
M Faried <m.a.faried@gmail.com>
2023-02-27 15:01:34 +0200

    Adding more resource links.

--- src/Resources.js
+++ src/Resources.js
@@ -3,11 +3,17 @@ const Resources = {
     footerLogo: `${process.env.PUBLIC_URL}/resources/footer-logo.png`,
     closeIcon: `${process.env.PUBLIC_URL}/resources/menu/close.png`,
     hamIcon: `${process.env.PUBLIC_URL}/resources/menu/hamburger.png`,
+    starIcon: `${process.env.PUBLIC_URL}/resources/menu/star.png`,
 
     menu: `${process.env.PUBLIC_URL}/resources/menu.webp`,
     heroImage: `${process.env.PUBLIC_URL}/resources/menu/item6.jpeg`,
     pannerImage: `${process.env.PUBLIC_URL}/resources/menu/item2.png`,
     errorImage: `${process.env.PUBLIC_URL}/resources/menu/item7.webp`,
+
+    greekSalad: `${process.env.PUBLIC_URL}/resources/menu/item7.webp`,
+    bruchetta: `${process.env.PUBLIC_URL}/resources/menu/item1.jpeg`,
+    lemonCake: `${process.env.PUBLIC_URL}/resources/menu/item8.webp`,
+
 };
 
 export default Resources;
