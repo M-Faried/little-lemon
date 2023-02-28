5241fbca7fd09946641c2f1069c92a76d069eed2
M Faried <m.a.faried@gmail.com>
2023-02-28 05:49:12 +0200

    Simplifying the page wrapper after relying on flex box for layout and stacking elements.

--- src/pages/PageWrapper/PageWrapper.js
+++ src/pages/PageWrapper/PageWrapper.js
@@ -2,30 +2,23 @@ import './PageWrapper.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 
-const PageHeader = ({ imgSrc, text }) => {
+const PageHeader = ({ imgSrc, title }) => {
     return (
         <header className='default-header'>
             <img src={imgSrc} alt="header" />
             <div className="header-text">
-                <h1>{text}</h1>
+                <h1>{title}</h1>
             </div>
         </header>
     )
 }
 
-const PageWrapper = (Component, imgSrc, text) => {
+const PageWrapper = (Component, imgSrc, title) => {
     return () => (
         <div className='page-structure'>
             <Navbar />
-            {imgSrc && text &&
-                <div>
-                    <PageHeader imgSrc={imgSrc} text={text} />
-                    <main><Component /></main>
-                </div>
-            }
-            {(!imgSrc || !text) &&
-                <main><Component /></main>
-            }
+            {imgSrc && title && <PageHeader imgSrc={imgSrc} title={title} />}
+            <main><Component /></main>
             <Footer />
         </div>
     )
